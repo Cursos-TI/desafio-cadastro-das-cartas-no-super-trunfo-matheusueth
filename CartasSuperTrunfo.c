444444cd95c06125265d572d268786ea8bcfd231
matheusueth <202502168144@alunos.estacio.br>
2025-03-29 19:05:47 +0000

    cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,17 +2,27 @@
 
 int main(){
 
-printf ("carta1");
-char ceara;
-char codigo;
-char cidade [10];
-int populacao;
-float pib;
-int pontos;
+char estado = 'rio de janeiro';
+char codigo[10]= "carta01";
+char cidade[20]="rio de janeiro";
+int populacao= 6000000;
+float area= 1.200;
+float pib= 949.3;
+int pontos= 58;
+
+char estado = 'sao paulo';
+char codigo[10]= "carta02";
+char cidade[20]="osasco";
+int populacao= 11895578;
+float area= 1.521;
+float pib= 3.000000000000;
+int pontos= 36;
 
 
 
 
+
+return 0;
 
 }
 
